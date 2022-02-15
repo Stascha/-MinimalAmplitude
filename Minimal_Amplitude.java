a30cbad0e7b3fa0661fae88c5ab5074353440f56
Stascha <stasarasic@gmail.com>
2022-02-15 12:58:43 +0100

    Update Minimal_Amplitude.java

--- Minimal_Amplitude.java
+++ Minimal_Amplitude.java
@@ -1,19 +1,58 @@
+import java.util.*;
 import java.util.Arrays;
 
-public class Main {
+public class Main 
+{	
+	public static int  minimal_amplitude(int[] a, int K)
+    {
+        int[] array_without_K_consecutive_el = new int[a.length - K];
+        int min_amp = 2147483647;
+        
+        for(int i = 0; i <= (a.length - K); i++)
+        {
+            int[] array1 = Arrays.copyOfRange(a, 0, i);
+            int[] array2 = Arrays.copyOfRange(a, i + K, a.length);
+            int temp_min_amp;
+         
+            int aLen = array1.length;
+            int bLen = array2.length;
+           
+            System.arraycopy(array1, 0, array_without_K_consecutive_el, 0, aLen);
+            System.arraycopy(array2, 0, array_without_K_consecutive_el, aLen, bLen);
+            
+            Arrays.sort(array_without_K_consecutive_el);
+            
+            temp_min_amp = array_without_K_consecutive_el[array_without_K_consecutive_el.length-1] - array_without_K_consecutive_el[0];
+            min_amp = temp_min_amp < min_amp ? temp_min_amp : min_amp;
+            
+            for(int n : array_without_K_consecutive_el)
+                System.out.print(n + " ");
+                
+            System.out.println();
+            System.out.print(temp_min_amp + " ");
+            System.out.println();
+            System.out.println();
+          
+        }
+        
+       return min_amp;
+        				
+	}
 
-    public static void main(String[] args) {
-        int[] array1 = {1, 2, 3};
-        int[] array2 = { 5, 6, 7};
+	public static void main(String args[])
+	{
+	    int min;
+		int[] a = {3, 1, 3, 9, 8, 3};
+		int K = 4;
+	
+	    min = minimal_amplitude(a, K);
 
-        int aLen = array1.length;
-        int bLen = array2.length;
-        int[] result = new int[aLen + bLen];
-
-        System.arraycopy(array1, 0, result, 0, aLen);
-        System.arraycopy(array2, 0, result, aLen, bLen);
-
-        for(int n : result)
-            System.out.print(n + " ");
-    }
+		System.out.print( " Minimal amplitude that can be obtained after the removal of " + K + " consecutive elements from A = " + min);
+	
+		   
+		
+		System.out.println(); 
+		
+	
+	}
 }
