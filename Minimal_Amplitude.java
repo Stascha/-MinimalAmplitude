2c45b1c38bfcba0864633cb6bca5075c0680b4e5
Stascha <stasarasic@gmail.com>
2022-02-18 18:55:50 +0100

    Update Minimal_Amplitude.java

--- Minimal_Amplitude.java
+++ Minimal_Amplitude.java
@@ -7,22 +7,36 @@ public class Main
     {
         int[] array_without_K_consecutive_el = new int[a.length - K];
         int min_amp = 2147483647;
+       
+      
         
         for(int i = 0; i <= (a.length - K); i++)
         {
+            int temp_min_amp;
+            int min = 2147483647;
+            int max = 0;
+            
             int[] array1 = Arrays.copyOfRange(a, 0, i);
             int[] array2 = Arrays.copyOfRange(a, i + K, a.length);
-            int temp_min_amp;
-         
+            
             int aLen = array1.length;
             int bLen = array2.length;
            
             System.arraycopy(array1, 0, array_without_K_consecutive_el, 0, aLen);
             System.arraycopy(array2, 0, array_without_K_consecutive_el, aLen, bLen);
             
-            Arrays.sort(array_without_K_consecutive_el);
             
-            temp_min_amp = array_without_K_consecutive_el[array_without_K_consecutive_el.length-1] - array_without_K_consecutive_el[0];
+            for(int n : array_without_K_consecutive_el)
+            {
+                if(min > n)
+                    min = n;
+                    
+                if(max < n)
+                    max = n;
+            }
+           
+            temp_min_amp = max - min;
+           
             min_amp = temp_min_amp < min_amp ? temp_min_amp : min_amp;
             
             for(int n : array_without_K_consecutive_el)
@@ -49,9 +63,30 @@ public class Main
 
        System.out.print( " Minimal amplitude that can be obtained after the removal of " + K + " consecutive elements from A = " + min);
 		
-       System.out.println(); 
+       System.out.println();
+       System.out.println();
+       
+       
+       int[] b = {3, 5, 1, 3, 9, 8};
+       K = 4;
+	
+       min = minimal_amplitude(b, K);
+
+       System.out.print( " Minimal amplitude that can be obtained after the removal of " + K + " consecutive elements from A = " + min);
+		
+       System.out.println();
+       System.out.println();
+       
+       
+       int[] c = {5, 3, 6, 1, 3};
+       K = 2;
+	
+       min = minimal_amplitude(c, K);
+
+       System.out.print( " Minimal amplitude that can be obtained after the removal of " + K + " consecutive elements from A = " + min);
+		
+       System.out.println();
 
    }
-
 
 }
